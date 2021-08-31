a7300fa6a4fa7703d48867bccc67e2f908a6a88d
emil <emalin2119@gmail.com>
2021-08-31 16:56:07 +0300

    change list item

--- src/components/listItem/index.tsx
+++ src/components/listItem/index.tsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 const ListFilmsItem = ({title, year, poster, id}: ListProps) => {
     return(
         <div className="col-3">
-            <Link to={`/film/${id}`}>
+            <Link to={`/Movies-App/film/${id}`}>
                 <div className="card">
                     <img src={poster} alt="" className="card__img"/>
                     <p className="card__title">
